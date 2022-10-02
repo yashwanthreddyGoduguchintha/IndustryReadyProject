83adcb828fce889f8904051d04ffdd38e08f6ad3
Shubham <jbshubham.outlook.com>
2022-10-02 16:58:44 +0530

    my new branch first commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 Print("hello world")
+x = input("Enter your first number")
+y = input("Enter your second number")
+print("addition of those number = ",(x+y))
+print("substraction of those number = ",(x-y))
+print("multiplication of those number = ",(x*y))
+if y != 0:
+    print("divison of those number = ", float((x/y)))
+else:
+    print("can not divide by zero")
